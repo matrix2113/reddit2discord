3aaaeb4afb09910e0600421bc835561653dd4bae
Andy <49132750+matrix2113@users.noreply.github.com>
2021-08-11 14:35:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ submission = reddit.submission("ohl82i")
 print(submission)
 
 embd = discord.Embed()
-embd.set_author(name=submission.author, icon_url=submission.author.icon_img, url=f"https://reddit.com/user/{submission.author.id}")
+embd.set_author(name=submission.author, icon_url=submission.author.icon_img, url=f"https://reddit.com/user/{submission.author}")
 embd.description = submission.title
 embd.title= submission.url
 webhook.send(embed=embd)
